59f982f56ca586df35a2014829703c42114c5e92
jjeongdong <donghun3296@naver.com>
2023-11-29 00:06:26 +0900

    feat : GlobalExceptionHandler 기능 추가

--- src/main/java/com/example/jwt_springsecurity/service/MemberService.java
+++ src/main/java/com/example/jwt_springsecurity/service/MemberService.java
@@ -4,7 +4,6 @@ import com.example.jwt_springsecurity.config.SecurityUtil;
 import com.example.jwt_springsecurity.domain.Member;
 import com.example.jwt_springsecurity.dto.MemberResponseDto;
 import com.example.jwt_springsecurity.repository.MemberRepository;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,21 +17,24 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public MemberResponseDto findMemberInfoById(Long memberId) {
-        Member member = memberRepository.findById(memberId).orElseThrow(EntityNotFoundException::new);
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new RuntimeException("유저 정보가 일치하지 않습니다."));
 
         return MemberResponseDto.of(member);
     }
 
     public MemberResponseDto findMemberInfoByEmail(String email) {
 
-        Member member = memberRepository.findById(SecurityUtil.getCurrentMemberId()).orElseThrow(() -> new RuntimeException("로그인 유저 정보가 없습니다."));
+        Member member = memberRepository.findById(SecurityUtil.getCurrentMemberId())
+                .orElseThrow(() -> new RuntimeException("로그인 유저 정보가 없습니다."));
 
         if (!Objects.equals(member.getEmail(), email)) {
             throw new RuntimeException("권한이 존재하지 않습니다.");
         }
 
-        Member DBmember = memberRepository.findByEmail(email).orElseThrow(EntityNotFoundException::new);
+        Member DBmember = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("유저 정보가 일치하지 않습니다."));
 
-        return MemberResponseDto.of(member);
+        return MemberResponseDto.of(DBmember);
     }
 }
