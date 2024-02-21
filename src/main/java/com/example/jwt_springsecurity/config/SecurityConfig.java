1992b1e252bf4863cbc87429481b30b51e2e3012
jjeongdong <donghun3296@naver.com>
2024-02-21 16:02:20 +0900

    chore : SecurityConfig 파일 설정

--- src/main/java/com/example/jwt_springsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/jwt_springsecurity/config/SecurityConfig.java
@@ -22,20 +22,23 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-            // CSRF 설정 Disable
-        http.csrf().disable()
-            // 시큐리티는 기본적으로 세션을 사용
-            // 여기서는 세션을 사용하지 않기 때문에 세션 설정을 Stateless 로 설정
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            // 로그인, 회원가입 API 는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
-            .and()
-            .authorizeRequests()
-            .requestMatchers("/auth/**").permitAll()
-            .anyRequest().authenticated()   // 나머지 API 는 전부 인증 필요
-            .and()
-            // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
-            .addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
+        // CSRF 설정 Disable
+        http
+                .csrf((auth) -> auth.disable());
+        // 시큐리티는 기본적으로 세션을 사용
+        // 여기서는 세션을 사용하지 않기 때문에 세션 설정을 Stateless 로 설정
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        // 로그인, 회원가입 API 는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
+        //경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/login/**").permitAll()
+                        .anyRequest().authenticated());
+        // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
+        http
+                .addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
--- src/main/java/com/example/jwt_springsecurity/config/TokenProvider.java
+++ src/main/java/com/example/jwt_springsecurity/config/TokenProvider.java
@@ -89,7 +89,10 @@ public class TokenProvider {
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
             return true;
         } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
             log.info("잘못된 JWT 서명입니다.");
